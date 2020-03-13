86257760f16a867a5c6966229718d757d4e01cf1
Luis Urrea <luisfernando12301985@gmail.com>
2020-03-13 11:46:07 -0500

    agregado todo el proyecto, primer commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_EF_Core.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
